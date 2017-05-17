367905dec12c349672e5290f37132784d5b1b64f
Gabsii <lukas.gabsi@tele2.at>
2017-05-17 11:13:23 +0200

    some stuff

--- reactjs/src/index.js
+++ reactjs/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Route, IndexRoute, HashRouter} from 'react-router-dom';
+import {Route, /*IndexRoute,*/ HashRouter} from 'react-router-dom';
 import App from './pages/Login';
 import './index.css';
 
