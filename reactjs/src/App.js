aeba9b853ccca19ef82935e061d1f65d946657ce
Jakob Fink <jakob.fink@student.htldornbirn.at>
2017-05-17 11:11:18 +0200

    Login update

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -26,18 +26,23 @@ class Login extends Component {
   render (){
     return(
       <div>
-        <div className="Inputlogin">
+        <div className="teamlogin">
           <Header/>
           <form>
             TEAMS<br/>
-              <input type="text" placeholder="Token"/>
-          </form>
-          <form>
-            PROJEKTLEITER<br/>
-            <input type="text" placeholder="E-Mail"/>
-            <input type="text" placeholder="Passwort"/>
+              <input type="text" placeholder="Token"/><br/>
+              <input type="submit" value="Sign in"/>
           </form>
         </div>
+        <div className="leaderlogin">
+          <form>
+            PROJECTLEADER<br/>
+            <input type="text" placeholder="E-Mail"/><br/>
+            <input type="text" placeholder="Passwort"/><br/>
+            <input type="submit" value="Sign in"/><br/>
+            <input type="submit" value="not registered?"/>
+          </form>
+          </div>
       </div>
 
     )
