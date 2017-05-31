d08ebdf005d5737f57f5898677b98e8cf03ee039
Jakob Fink <jakob.fink@student.htldornbirn.at>
2017-05-31 11:16:10 +0200

    login
    changed size of buttons & inputfields

---
+++ reactjs/src/pages/createProject.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+
+class App extends React.Component {
+  render() {
+      return (
+         <div className="row">
+            <Header/>
+         </div>
+      );
+   }
+}
